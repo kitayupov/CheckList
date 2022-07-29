4e50f14169ffb29ebab64a47bfb17227ceff5ecc
kamil.tayupov <k-i-tayupov@yandex-team.ru>
2022-07-29 11:23:31 +0300

    show example list

--- app/src/main/java/ru/devkit/shoppinglist/MainActivity.kt
+++ app/src/main/java/ru/devkit/shoppinglist/MainActivity.kt
@@ -2,10 +2,33 @@ package ru.devkit.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import ru.devkit.shoppinglist.model.ShoppingListItemModel
+import ru.devkit.shoppinglist.presentation.ShoppingListAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private val list = mutableListOf(
+        ShoppingListItemModel("potato"),
+        ShoppingListItemModel("tomato"),
+        ShoppingListItemModel("soup"),
+        ShoppingListItemModel("banana"),
+        ShoppingListItemModel("milk"),
+        ShoppingListItemModel("bread"),
+    )
+
+    private val adapter = ShoppingListAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = adapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adapter.list = list
     }
 }
