eb40aa083e87f771e19dd6769f2ca70203e36533
kamil.tayupov <k-i-tayupov@yandex-team.ru>
2022-08-04 22:02:02 +0300

    move initializations to app

--- app/src/main/java/ru/devkit/shoppinglist/App.kt
+++ app/src/main/java/ru/devkit/shoppinglist/App.kt
@@ -1,5 +1,18 @@
 package ru.devkit.shoppinglist
 
 import android.app.Application
+import ru.devkit.shoppinglist.data.db.ProductsDatabase
+import ru.devkit.shoppinglist.data.preferences.PreferencesProvider
+import ru.devkit.shoppinglist.data.repository.ProductsRepository
+import ru.devkit.shoppinglist.data.source.ProductsDataSource
+import ru.devkit.shoppinglist.domain.DataModelStorageInteractor
+import ru.devkit.shoppinglist.ui.presentation.ShoppingListPresenter
 
-class App : Application()
+class App : Application() {
+    private val database by lazy { ProductsDatabase.getInstance(this) }
+    private val dataSource by lazy { ProductsDataSource(database) }
+    private val repository by lazy { ProductsRepository(dataSource) }
+    private val interactor by lazy { DataModelStorageInteractor(repository) }
+    private val preferences by lazy { PreferencesProvider(this) }
+    val presenter by lazy { ShoppingListPresenter(interactor, preferences) }
+}
--- app/src/main/java/ru/devkit/shoppinglist/MainActivity.kt
+++ app/src/main/java/ru/devkit/shoppinglist/MainActivity.kt
@@ -7,23 +7,17 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
-import ru.devkit.shoppinglist.data.db.ProductsDatabase
 import ru.devkit.shoppinglist.data.model.ListItemDataModel
-import ru.devkit.shoppinglist.data.preferences.PreferencesProvider
-import ru.devkit.shoppinglist.data.repository.ProductsRepository
-import ru.devkit.shoppinglist.data.source.ProductsDataSource
-import ru.devkit.shoppinglist.domain.DataModelStorageInteractor
 import ru.devkit.shoppinglist.ui.adapter.ShoppingListAdapter
 import ru.devkit.shoppinglist.ui.additem.CreateNewItemViewRouter
 import ru.devkit.shoppinglist.ui.confirmation.ConfirmationDialogFragment
 import ru.devkit.shoppinglist.ui.model.ListItemUiModel
 import ru.devkit.shoppinglist.ui.presentation.ShoppingListContract
-import ru.devkit.shoppinglist.ui.presentation.ShoppingListPresenter
 
 class MainActivity : AppCompatActivity() {
 
     private val createNewItemViewRouter = CreateNewItemViewRouter(supportFragmentManager)
-    private lateinit var presenter: ShoppingListPresenter
+    private val presenter by lazy { (application as App).presenter }
     private val adapter = ShoppingListAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +25,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setupRecyclerView()
         setupActionButton()
-        createStorage()
     }
 
     override fun onResume() {
@@ -63,15 +56,6 @@ class MainActivity : AppCompatActivity() {
         val dialog = ConfirmationDialogFragment.newInstance(getString(R.string.dialog_clear_data_title))
         dialog.confirmAction = presenter::clearData
         dialog.show(supportFragmentManager, ConfirmationDialogFragment.TAG)
-    }
-
-    private fun createStorage() {
-        val database = ProductsDatabase.getInstance(this)
-        val dataSource = ProductsDataSource(database)
-        val repository = ProductsRepository(dataSource)
-        val interactor = DataModelStorageInteractor(repository)
-        val preferences = PreferencesProvider(this)
-        presenter = ShoppingListPresenter(interactor, preferences)
     }
 
     private fun setupRecyclerView() {
