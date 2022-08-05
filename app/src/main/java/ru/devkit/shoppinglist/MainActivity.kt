efd2334b5ed5a288ae5972d3bedea2320eccb8ec
kamil.tayupov <k-i-tayupov@yandex-team.ru>
2022-08-05 12:47:19 +0300

    show action mode menu

--- app/src/main/java/ru/devkit/shoppinglist/MainActivity.kt
+++ app/src/main/java/ru/devkit/shoppinglist/MainActivity.kt
@@ -5,6 +5,8 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.ActionMode
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import ru.devkit.shoppinglist.data.model.ListItemDataModel
@@ -20,9 +22,12 @@ class MainActivity : AppCompatActivity() {
     private val presenter by lazy { (application as App).presenter }
     private val adapter = ShoppingListAdapter()
 
+    private val toolbar by lazy { findViewById<Toolbar>(R.id.toolbar) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
         setupRecyclerView()
         setupActionButton()
     }
@@ -84,6 +89,41 @@ class MainActivity : AppCompatActivity() {
 
         override fun selectionMode(checked: Boolean) {
             adapter.selectionMode = checked
+            if (checked) {
+                startSupportActionMode(actionModeCallback)
+            } else {
+                actionModeCallback.mode?.finish()
+            }
+        }
+    }
+
+    private val actionModeCallback = object : ActionMode.Callback {
+
+        var mode: ActionMode? = null
+
+        override fun onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean {
+            return when (item.itemId) {
+                R.id.menu_contextual_delete -> {
+                    // delete selected
+                    true
+                }
+                else -> return false
+            }
+        }
+
+        override fun onCreateActionMode(mode: ActionMode, menu: Menu): Boolean {
+            menuInflater.inflate(R.menu.menu_contextual, menu)
+            this.mode = mode
+            return true
+        }
+
+        override fun onPrepareActionMode(mode: ActionMode, menu: Menu): Boolean {
+            // no-op
+            return false
+        }
+
+        override fun onDestroyActionMode(mode: ActionMode) {
+            // no-op
         }
     }
 }
