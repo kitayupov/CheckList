79f6963c2bdf6157a9812abb4d0db874b1423b74
kamil.tayupov <k-i-tayupov@yandex-team.ru>
2022-08-06 17:24:23 +0300

    get text on enter action

--- app/src/main/java/ru/devkit/checklist/ui/createitem/CreateItemBottomSheetFragment.kt
+++ app/src/main/java/ru/devkit/checklist/ui/createitem/CreateItemBottomSheetFragment.kt
@@ -2,6 +2,7 @@ package ru.devkit.checklist.ui.createitem
 
 import android.content.DialogInterface
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,14 +21,27 @@ class CreateItemBottomSheetFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val editText = view.findViewById<EditText>(R.id.input_text)
+        editText.setOnKeyListener { _, _, event ->
+            if (event.keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN) {
+                getText(editText)
+            }
+            true
+        }
+
         val createButton = view.findViewById<View>(R.id.create_button)
         createButton.setOnClickListener {
-            val text = editText.text.trim().takeIf(CharSequence::isNotBlank) ?: return@setOnClickListener
-            createAction.invoke(text.toString())
-            editText.text = null
+            getText(editText)
         }
+
         editText.requestFocus()
+    }
+
+    private fun getText(editText: EditText) {
+        val text = editText.text.trim().takeIf(CharSequence::isNotBlank) ?: return
+        createAction.invoke(text.toString())
+        editText.text = null
     }
 
     override fun onCancel(dialog: DialogInterface) {
