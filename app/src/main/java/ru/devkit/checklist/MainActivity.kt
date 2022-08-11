8952b03268b691b0c9fbe4d4bcc1f38a8ceb4034
kamil.tayupov <k-i-tayupov@yandex-team.ru>
2022-08-11 21:56:36 +0300

    move everything to fragment

--- app/src/main/java/ru/devkit/checklist/MainActivity.kt
+++ app/src/main/java/ru/devkit/checklist/MainActivity.kt
@@ -1,11 +1,7 @@
 package ru.devkit.checklist
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.view.ActionMode
 import androidx.appcompat.widget.Toolbar
 import ru.devkit.checklist.ui.presentation.CheckListFragment
 
@@ -30,7 +26,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupCheckList() {
         val fragment = CheckListFragment().apply {
-            callback = this@MainActivity.callback
             presenter = this@MainActivity.presenter
             router = this@MainActivity.router
         }
@@ -42,92 +37,5 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         router.detach()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_actions, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.menu_actions_clear_data -> {
-                router.showClearAllConfirmation(presenter::clearData)
-                true
-            }
-            R.id.menu_actions_sort_ranking -> {
-                presenter.setSortRanking()
-                true
-            }
-            R.id.menu_actions_sort_default -> {
-                presenter.setSortDefault()
-                true
-            }
-            R.id.menu_actions_sort_name -> {
-                presenter.setSortName()
-                true
-            }
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
-    private val callback = object : CheckListFragment.Callback {
-        override fun onSelectionMode(checked: Boolean) {
-            if (checked) {
-                startSupportActionMode(actionModeCallback)
-            } else {
-                actionModeCallback.mode?.finish()
-            }
-        }
-
-        override fun onShowSelectionCount(count: Int) {
-            actionModeCallback.mode?.title = getString(R.string.action_mode_selected_count, count)
-        }
-
-        override fun onShowMessage(text: String) {
-            Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private val actionModeCallback = object : ActionMode.Callback {
-
-        var mode: ActionMode? = null
-
-        override fun onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean {
-            return when (item.itemId) {
-                R.id.menu_contextual_delete -> {
-                    router.showRemoveSelectedConfirmation(presenter::removeSelected)
-                    true
-                }
-                R.id.menu_contextual_select_all -> {
-                    presenter.selectAll()
-                    true
-                }
-                R.id.menu_contextual_check_selected -> {
-                    presenter.checkSelected()
-                    true
-                }
-                R.id.menu_contextual_uncheck_selected -> {
-                    presenter.uncheckSelected()
-                    true
-                }
-                else -> return false
-            }
-        }
-
-        override fun onCreateActionMode(mode: ActionMode, menu: Menu): Boolean {
-            menuInflater.inflate(R.menu.menu_contextual, menu)
-            this.mode = mode
-            return true
-        }
-
-        override fun onPrepareActionMode(mode: ActionMode, menu: Menu): Boolean {
-            // no-op
-            return false
-        }
-
-        override fun onDestroyActionMode(mode: ActionMode) {
-            presenter.clearSelected()
-        }
     }
 }
--- app/src/main/java/ru/devkit/checklist/ui/presentation/CheckListFragment.kt
+++ app/src/main/java/ru/devkit/checklist/ui/presentation/CheckListFragment.kt
@@ -2,7 +2,13 @@ package ru.devkit.checklist.ui.presentation
 
 import android.content.Context
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.ActionMode
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
@@ -25,8 +31,6 @@ class CheckListFragment : Fragment(R.layout.fragment_check_list), CheckListContr
     var presenter: CheckListPresenter? = null
     var router: CheckListRouter? = null
 
-    var callback: Callback? = null
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupRecyclerView(view)
@@ -36,12 +40,13 @@ class CheckListFragment : Fragment(R.layout.fragment_check_list), CheckListContr
     override fun onAttach(context: Context) {
         super.onAttach(context)
         presenter?.attachView(this)
+        floatingActionButton?.show()
     }
 
     override fun onDetach() {
         super.onDetach()
         presenter?.detachView()
-        callback = null
+        floatingActionButton?.hide()
     }
 
     private fun setupRecyclerView(view: View) {
@@ -72,26 +77,89 @@ class CheckListFragment : Fragment(R.layout.fragment_check_list), CheckListContr
     }
 
     override fun setSelectionMode(checked: Boolean) {
-        callback?.onSelectionMode(checked)
         adapter.selectionMode = checked
         if (checked) {
+            (activity as? AppCompatActivity)?.startSupportActionMode(actionModeCallback)
             floatingActionButton?.hide()
         } else {
+            actionModeCallback.mode?.finish()
             floatingActionButton?.show()
         }
     }
 
     override fun showSelectedCount(value: Int) {
-        callback?.onShowSelectionCount(value)
+        actionModeCallback.mode?.title = getString(R.string.action_mode_selected_count, value)
     }
 
     override fun showMessage(text: String) {
-        callback?.onShowMessage(text)
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
     }
 
-    interface Callback {
-        fun onSelectionMode(checked: Boolean)
-        fun onShowSelectionCount(count: Int)
-        fun onShowMessage(text: String)
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_actions, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_actions_clear_data -> {
+                router?.showClearAllConfirmation { presenter?.clearData() }
+                true
+            }
+            R.id.menu_actions_sort_ranking -> {
+                presenter?.setSortRanking()
+                true
+            }
+            R.id.menu_actions_sort_default -> {
+                presenter?.setSortDefault()
+                true
+            }
+            R.id.menu_actions_sort_name -> {
+                presenter?.setSortName()
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    private val actionModeCallback = object : ActionMode.Callback {
+
+        var mode: ActionMode? = null
+
+        override fun onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean {
+            return when (item.itemId) {
+                R.id.menu_contextual_delete -> {
+                    router?.showRemoveSelectedConfirmation { presenter?.removeSelected() }
+                    true
+                }
+                R.id.menu_contextual_select_all -> {
+                    presenter?.selectAll()
+                    true
+                }
+                R.id.menu_contextual_check_selected -> {
+                    presenter?.checkSelected()
+                    true
+                }
+                R.id.menu_contextual_uncheck_selected -> {
+                    presenter?.uncheckSelected()
+                    true
+                }
+                else -> return false
+            }
+        }
+
+        override fun onCreateActionMode(mode: ActionMode, menu: Menu): Boolean {
+            activity?.menuInflater?.inflate(R.menu.menu_contextual, menu)
+            this.mode = mode
+            return true
+        }
+
+        override fun onPrepareActionMode(mode: ActionMode, menu: Menu): Boolean {
+            // no-op
+            return false
+        }
+
+        override fun onDestroyActionMode(mode: ActionMode) {
+            presenter?.clearSelected()
+        }
     }
 }
