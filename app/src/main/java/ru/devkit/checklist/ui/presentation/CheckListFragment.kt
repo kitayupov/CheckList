25b21335ada6d93124c9a068087d9d11e8300802
kamil.tayupov <k-i-tayupov@yandex-team.ru>
2022-08-11 19:38:35 +0300

    create empty fragment

---
+++ app/src/main/java/ru/devkit/checklist/ui/presentation/CheckListFragment.kt
@@ -0,0 +1,8 @@
+package ru.devkit.checklist.ui.presentation
+
+import androidx.fragment.app.Fragment
+import ru.devkit.checklist.R
+
+class CheckListFragment : Fragment(R.layout.fragment_check_list) {
+
+}
